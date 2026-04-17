16abc60d3d3feeb4b5e410fbed993f367d90d559
Vrunnx <jjorgesilv@gmail.com>
2026-04-17 10:23:00 -0300

    Delete nc-snapshot-rs directory

--- nc-snapshot-rs/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod chain_cleanup;
-pub mod prune;
-pub mod state_trie;
-pub mod trie;
--- nc-snapshot-rs/src/trie/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod bencodex;
-pub mod bloom;
-pub mod chain_reader;
-pub mod node;
-pub mod pruner;
-pub mod sst_writer;
